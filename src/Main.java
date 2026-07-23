c76e32e8de3dce20ab114e276e9dd69694327bdf
Mohamed-El-Khayat <mohamedelkhayat005@gmail.com>
2026-07-24 02:50:56 +0300

    Update main interface

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,20 @@
 void main() {
-   Vehicle v = new Vehicle("ABC123", "23/7/2026", "Truck", 60, false);
+   Scanner input = new Scanner(System.in);
+   System.out.println("Plate Number: ");
+   String plateNumber = input.nextLine();
+
+   System.out.println("Date: ");
+   String date = input.nextLine();
+
+   System.out.println("Vehicle Type: ");
+   String vehicleType = input.nextLine();
+
+   System.out.println("Speed: ");
+   double speed = input.nextDouble();
+
+   System.out.println("Seatbelt Fastened: ");
+   boolean seatbelt = input.nextBoolean();
+
+   Vehicle v = new Vehicle(plateNumber, date, vehicleType, speed, seatbelt);
    Radar.trafficFine(v);
 }
